2debcf00884ca64f680e193d291d268b19ba6388
KonakovBG <svetoslav.konakov@abv.bg>
2021-03-16 15:57:36 +0200

    Getting information from server

--- php/booking.php
+++ php/booking.php
@@ -1,12 +1,13 @@
 <?php 
-	$data = [];
+	$data = [
+		'fname' => $_POST['fname'],
+		'lname' => $_POST['lname'],
+		'pin' => $_POST['pin'],
+		'email' => $_POST['email'],
+		'room' => $_POST['room'],
+		'people' => $_POST['people']
 
-	foreach ($_POST as $value) {
-		var_dump($value); 
-		$data[] = $value;
-	}
-
-	var_dump($_POST['room']);
+	];
 
 	echo json_encode($data);
 ?>
